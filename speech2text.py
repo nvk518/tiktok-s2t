3290d6392980604007ab50c2ffa2f292c7ac12b1
nvk518 <nvk5181@gmail.com>
2024-03-24 09:47:41 -0700

    updated download tiktok url

--- speech2text.py
+++ speech2text.py
@@ -1,5 +1,6 @@
 import requests
 import re
+
 # from google.colab import drive
 from moviepy.editor import *
 import whisper
@@ -9,35 +10,41 @@ from google.oauth2 import service_account
 
 # drive.mount('/content/drive')
 
-def download_tiktok(url="https://tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com/vid/index"):
-    querystring = {"url":"https://www.tiktok.com/@_product_finder/video/7336640884626296069"}
+
+def download_tiktok(url):
+    querystring = {"url": url}
 
     headers = {
-      "X-RapidAPI-Key": "7b0ea31ba2msh3cc78d4a4525496p189cc8jsn8fcbfd95ffc2",
-      "X-RapidAPI-Host": "tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com"
+        "X-RapidAPI-Key": "7b0ea31ba2msh3cc78d4a4525496p189cc8jsn8fcbfd95ffc2",
+        "X-RapidAPI-Host": "tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com",
     }
 
-    response = requests.get(url, headers=headers, params=querystring)
+    response = requests.get(
+        "https://tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com/vid/index",
+        headers=headers,
+        params=querystring,
+    )
 
     print(response.json())
-    video_url = response.json()['video'][0]
+    video_url = response.json()["video"][0]
 
     response = requests.get(video_url)
 
     if response.status_code == 200:
-        with open('downloaded_video.mp4', 'wb') as file:
+        with open("downloaded_video.mp4", "wb") as file:
             file.write(response.content)
         print("Video downloaded successfully.")
     else:
         print(f"Failed to download video. Status code: {response.status_code}")
 
+
 def obtain_audio(file_path):
 
-	# Replace 'your_video.mp4' with the path to your video file
-	video = VideoFileClip(file_path)
+    # Replace 'your_video.mp4' with the path to your video file
+    video = VideoFileClip(file_path)
 
-	# Replace 'output_audio.mp3' with the desired output MP3 file name
-	video.audio.write_audiofile("output_audio.wav")
+    # Replace 'output_audio.mp3' with the desired output MP3 file name
+    video.audio.write_audiofile("output_audio.wav")
 
 
 def execute_transcription():
@@ -56,9 +63,11 @@ def execute_transcription():
 def execute_gpt(text):
     llm = OpenAI(api_key="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")
 
-    transcribed_text = f'{text}'
+    transcribed_text = f"{text}"
 
-    prompt = [f"Identify all restaurants/attractions mentioned in the following tiktok audio transcript with city, state, country they are located in: {transcribed_text}. If place name is unclear (ie. the text transcript says Booted In, but could instead be Boudin Bakery in San Francisco), try fixing it. provide what the transcript says was a recommended item. Provide your response in this strict format: 'Name: _, Location: _, Notes: _'"]
+    prompt = [
+        f"Identify all restaurants/attractions mentioned in the following tiktok audio transcript with city, state, country they are located in: {transcribed_text}. If place name is unclear (ie. the text transcript says Booted In, but could instead be Boudin Bakery in San Francisco), try fixing it. provide what the transcript says was a recommended item. Provide your response in this strict format: 'Name: _, Location: _, Notes: _'"
+    ]
 
     response = llm.generate(prompt)
 
@@ -67,10 +76,12 @@ def execute_gpt(text):
     locations = output.split("\n")
     return locations
 
+
 # Sheets API setup
-SHEETS_SERVICE_ACCOUNT_FILE = './googlesheets_pk.json'
-SPREADSHEET_ID = '1UCq8qQIRUNBbtwAV4tZAUu-2sxPyNGeQaqgt9ODD8LY'
-SHEET_NAME = 'Sheet2'
+SHEETS_SERVICE_ACCOUNT_FILE = "./googlesheets_pk.json"
+SPREADSHEET_ID = "1UCq8qQIRUNBbtwAV4tZAUu-2sxPyNGeQaqgt9ODD8LY"
+SHEET_NAME = "Sheet2"
+
 
 def update_sheet(locations):
     rows_to_insert = []
@@ -82,21 +93,27 @@ def update_sheet(locations):
         notes = split_notes[1]
         rows_to_insert.append([name, location, notes])
 
-    credentials = service_account.Credentials.from_service_account_file(SHEETS_SERVICE_ACCOUNT_FILE)
-    service = build('sheets', 'v4', credentials=credentials)
+    credentials = service_account.Credentials.from_service_account_file(
+        SHEETS_SERVICE_ACCOUNT_FILE
+    )
+    service = build("sheets", "v4", credentials=credentials)
 
-    request_body = {
-        'values': rows_to_insert
-    }
-    response = service.spreadsheets().values().append(
-        spreadsheetId=SPREADSHEET_ID,
-        range=f'{SHEET_NAME}!A:D',
-        valueInputOption='USER_ENTERED',
-        insertDataOption='INSERT_ROWS',
-        body=request_body
-    ).execute()
+    request_body = {"values": rows_to_insert}
+    response = (
+        service.spreadsheets()
+        .values()
+        .append(
+            spreadsheetId=SPREADSHEET_ID,
+            range=f"{SHEET_NAME}!A:D",
+            valueInputOption="USER_ENTERED",
+            insertDataOption="INSERT_ROWS",
+            body=request_body,
+        )
+        .execute()
+    )
 
     print("Update Complete. Response:", response)
+
 
 # !pip install -q streamlit
 
@@ -135,6 +152,7 @@ def update_sheet(locations):
 # update_sheet(locations)
 import streamlit as st
 
+
 def main():
     st.title("TikTok Video Processor")
 
@@ -146,11 +164,15 @@ def main():
             download_tiktok(url)
             obtain_audio("./downloaded_video.mp4")
             text = execute_transcription()
-            locations = execute_gpt(text)
-            update_sheet(locations)
-            st.success("Processing completed.")
+            if text:
+                locations = execute_gpt(text)
+                update_sheet(locations)
+                st.success("Processing completed.")
+            else:
+                st.error("Errored while executing audio transcription.")
         else:
             st.error("Please enter a URL.")
+
 
 if __name__ == "__main__":
     main()
